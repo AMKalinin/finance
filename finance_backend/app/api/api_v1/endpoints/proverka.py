f720688b06c49892ee158d6b9188f31b6e819b46
akalinin <am1kalinin@gmail.com>
2024-04-03 16:12:33 +0300

    start project

--- finance_backend/app/api/api_v1/endpoints/proverka.py
+++ finance_backend/app/api/api_v1/endpoints/proverka.py
@@ -6,6 +6,7 @@ router = APIRouter()
 @router.get('')
 def get_test(test:str):
     a = 1
+    v = 10
     return {test}
 
 def fjsdkfjkasodfjosjdfkjksdjfklasdfmdmflsmdfklsdmfklasmdkflsmdkflamsdkflczvaeoriqoituwemvcfkkk():
