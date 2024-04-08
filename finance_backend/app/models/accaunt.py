ca59b89bb783e4c55c4e74b055ef21b723aa83eb
akalinin <am1kalinin@gmail.com>
2024-04-08 12:30:27 +0300

    add balance in db model account

--- finance_backend/app/models/accaunt.py
+++ finance_backend/app/models/accaunt.py
@@ -8,5 +8,6 @@ from app.db.base_class import Base
 
 class Accaunt(Base):
     id: Mapped[uuid.UUID] = mapped_column(types.Uuid, primary_key=True, default=uuid.uuid4)
+    balance: Mapped[float] = mapped_column()
     name: Mapped[str] = mapped_column(Text)
     description: Mapped[str] = mapped_column(Text)
