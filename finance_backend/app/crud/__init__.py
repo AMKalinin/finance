bb246e0f5def2be7a31ef81b96c7e9d62dcef4d9
akalinin <am1kalinin@gmail.com>
2024-04-08 16:17:46 +0300

    add crud and api route for category

--- finance_backend/app/crud/__init__.py
+++ finance_backend/app/crud/__init__.py
@@ -1,1 +1,2 @@
 from .crud_account import account  # noqa
+from .crud_category import category  # noqa
--- finance_backend/app/schemas/category.py
+++ finance_backend/app/schemas/category.py
@@ -5,6 +5,11 @@ class category_in(BaseModel):
     name: str
 
 
+class category_in_name(BaseModel):
+    id: int
+    name: str
+
+
 class category_out(BaseModel):
     id: int
     name: str
