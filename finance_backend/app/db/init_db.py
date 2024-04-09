9a6372f4c36c3808f1c5b2fa0a1f2c00fc391b65
AMKalinin <am1kalinin@gmail.com>
2024-04-09 15:29:25 +0300

    change name for Type tabel on Type_transaction

--- finance_backend/app/db/init_db.py
+++ finance_backend/app/db/init_db.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session  # noqa
 from app.models.account import Account  # noqa
 from app.models.category import Category  # noqa
 from app.models.transaction import Transaction  # noqa
-from app.models.type import Type  # noqa
+from app.models.type_transaction import Type_transaction  # noqa
 
 from .base_class import Base
 from .session import engine
@@ -14,14 +14,21 @@ def init_db():
 
     with Session(engine) as session:
         with session.begin():
-            type_debit = session.query(Type).filter(Type.name == "debit").first()
+            type_debit = (
+                session.query(Type_transaction).filter(Type_transaction.name == "Debit").first()
+            )
             if not type_debit:
-                type_debit = Type(name="debit", description="debit списание")  # type: ignore
-
+                type_debit = Type_transaction(
+                    name="Debit", description="debit / списание"
+                )  # type: ignore
                 session.add(type_debit)
 
-                type_perevod = Type(name="perevod", description="perevod перевод")  # type: ignore
-                session.add(type_perevod)
+                type_transfer = Type_transaction(
+                    name="Transfer", description="Transfer / перевод"
+                )  # type: ignore
+                session.add(type_transfer)
 
-                type_popolnenie = Type(name="popolnenie", description="пополнение")  # type: ignore
-                session.add(type_popolnenie)
+                type_adding = Type_transaction(
+                    name="Adding", description="Adding / пополнение"
+                )  # type: ignore
+                session.add(type_adding)
--- finance_backend/app/models/type_transaction.py
+++ finance_backend/app/models/type_transaction.py
@@ -4,8 +4,7 @@ from sqlalchemy.orm import Mapped, mapped_column
 from app.db.base_class import Base
 
 
-class Type(Base):
+class Type_transaction(Base):
     # Пополнение\ списание \ перевод
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(Text)
+    name: Mapped[str] = mapped_column(primary_key=True)
     description: Mapped[str] = mapped_column(Text)
