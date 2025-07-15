0ff4fbf90718fe681294b2d65a217950fa3dbad1
AMKalinin <am1kalinin@gmail.com>
2025-07-15 21:35:48 +0300

    update db models user

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,5 @@
 import uuid
-
+from datetime import date
 from sqlalchemy import Text, types
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -9,6 +9,8 @@ from app.db.base_class import Base
 class User(Base):
     id: Mapped[uuid.UUID] = mapped_column(types.Uuid, primary_key=True, default=uuid.uuid4)
     description: Mapped[str] = mapped_column(Text)
+    subscription_type: Mapped[str] = mapped_column(default='free')
+    subscription_expiry: Mapped[date] = mapped_column(nullable=True)
 
     accounts = relationship("Account", back_populates="user", lazy="dynamic")
     categories = relationship("Category", back_populates="user", lazy="dynamic")
