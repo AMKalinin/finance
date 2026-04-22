bc4aa9f3b4f28a793845426820399aa3af980374
AMKalinin <am1kalinin@gmail.com>
2026-04-22 22:19:23 +0300

    delete print

--- backend/app/api/api_v1/endpoints/transaction.py
+++ backend/app/api/api_v1/endpoints/transaction.py
@@ -23,9 +23,7 @@ router = APIRouter()
 
 @router.get("/all", response_model=list[transaction_out])
 def get_all(fin_app: Fin_app = Depends(deps.get_fin_service)):
-    res = fin_app.get_all_transaction()
-    [print(position.transaction_id) for position in res[1].positions]
-    return res # crud.transaction.get_all_transaction(db)
+    return fin_app.get_all_transaction() # crud.transaction.get_all_transaction(db)
 
 
 @router.get("/by_period", response_model=list[transaction_out])
